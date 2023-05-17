b6322eec03ef28a1a3fdf49c3b144b44a0167e6c
Ahmad urabi <hamod9041@gmail.com>
2023-05-17 17:56:19 +0300

    Products Route Complete

--- src/main.rs
+++ src/main.rs
@@ -4,89 +4,50 @@ use rocket::serde::json::Json;
 use rocket::serde::Serialize;
 use rocket::serde::Deserialize;
 
-#[derive(Serialize, Deserialize)]
-struct SUPPLIER {
-    SupplierID: String,
-    Name: String,
-    Address: String,
-    Phone: String,
-    Email: String
-}
-
-#[derive(Serialize, Deserialize)]
-struct BRANCH {
-    BranchID: String,
-    Name: String,
-    City: String,
-    Country : String,
-    Street: String,
-    ManagerSSN: String,
-}
-
-#[derive(Serialize, Deserialize)]
-struct EMPLOYEE {
-    Ssn: String,
-    FName: String,
-    LName: String,
-    Country: String,
-    City: String,
-    Street: String,
-    Salary: String,
-    BranchID: String,
-    Sex: String,
-    Type: String,
-    BDate: String
-}
-
-#[derive(Serialize, Deserialize)]
-struct DEPENDANT{
-    Name: String,
-    Sex: String,
-    BDate: String,
-    Relationship: String,
-    EmployeeSSN: String
-}
 
 #[derive(Serialize, Deserialize)]
 struct Product {
-    ProductID: String,
-    ProductName: String,
-    Quantity: String,
-    Price: String,
-    Brand: String,
-    BranchID: String,
-    SupplierID: String
+    ProductID: Option<String>,
+    ProductName: Option<String>,
+    Quantity: Option<String>,
+    Price: Option<String>,
+    Brand: Option<String>,
+    BranchID: Option<String>,
+    SupplierID: Option<String>
 }
-
-#[derive(Serialize, Deserialize)]
-struct STOCK {
-    ProductID: String,
-    BranchID: String,
-    Quantity: String,
-    BranchName:String
-}
+/*
+let productSQL = format!("SELECT * FROM INVENTORY WHERE PRODUCTID = '{}'", productID);
+*/
 
 
 #[derive(serde::Deserialize)]
-struct Params {
-    pRef: Option<String>,
+struct ParamsProducts {
+    pID: Option<String>,
+    pBrand: Option<String>,
+    pName: Option<String>,
 }
 
 
+
 #[post("/products", data = "<params>")]
-async fn post(params: Json<Params>) -> Option<Json<Vec<PRODUCT>>> {
+async fn post(params: Json<ParamsProducts>) -> Option<Json<Vec<Product>>> {
    
     let s = getProduct(params).unwrap();
     if s.is_empty() {
         None
     } else {
-        Some(Json(s.into_iter().map(|i| PRODUCT {
-        ITEM_ID : i.ITEM_ID.clone(),
-        SECOND_DISC_PER_STORE_35 : i.SECOND_DISC_PER_STORE_35.clone(),
+        Some(Json(s.into_iter().map(|i| Product {
+            ProductID: i.ProductID.clone(),
+            ProductName: i.ProductName.clone(),
+            Quantity: i.Quantity.clone(),
+            Price: i.Price.clone(),
+            Brand: i.Brand.clone(),
+            BranchID: i.BranchID.clone(),
+            SupplierID: i.SupplierID.clone(),
     }).collect()))
 }
 }
-
+/*
 #[post("/new", data = "<params>")]
 async fn postEmp(params: Json<Params>) -> Boolean {
    
@@ -97,56 +58,65 @@ async fn postEmp(params: Json<Params>) -> Boolean {
         false
     }
 }
-
+*/
 
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![post])
 }
 
-fn getProducts(params: Json<Params>) -> Result<Vec<PRODUCT>> {
+fn getProduct(params: Json<ParamsProducts>) -> Result<Vec<Product>> {
     let username = "odbc_jhc";
     let password = "odbc_jhc";
     let database = "//10.0.0.21:1521/a12";
-    let mut mypRef = "%";
-    let mut mypBarcode = "%";
-    let mut mypId = "%";
-    if let Some(pRef) = &params.pRef {
-        mypRef = pRef;
+
+    let mut mypID = "%";
+    let mut mypBrand = "%";
+    let mut mypName = "%";
+    if let Some(pID) = &params.pID {
+        mypID = pID;
     }
 
-    if let Some(pBarcode) = &params.pBarcode {
-        mypBarcode = pBarcode;
+    if let Some(pBrand) = &params.pBrand {
+        mypBrand = pBrand;
     }
 
-    if let Some(pId) = &params.pId {
-        mypId = pId;
+    if let Some(pName) = &params.pName {
+        mypName = pName;
     }
 
-    let sql = format!("SELECT ITEM_ID, IS_ACTIVE, CAN_BE_SOLD, ITEM_DESC, ITEM_DESC_S, FOREIGN_ITEM_CODE, ITEM_CAT, ITEM_SUB_CAT, SALE_UNIT, UNIT_DESC, PACKING, CARD_OPEN_DATE, HS_CODE, COUNTRY, COUNTRY_DESC, SUPPLIER_ID, SUPPLIER_DESC, ITEM_MAIN_BARCODE, NATURE_ID, NATURE_DESC, TRADE_ID, TRADE_DESC, QTY_STORE_01, QTY_STORE_02, QTY_STORE_05, QTY_STORE_06, QTY_STORE_07, QTY_STORE_08, QTY_STORE_10, QTY_STORE_11, QTY_STORE_12, QTY_STORE_19, QTY_STORE_21, QTY_STORE_23, QTY_STORE_31, QTY_STORE_32, QTY_STORE_33, QTY_STORE_34, QTY_STORE_35, SALE_PRICE_NOTAX_STORE_01, SALE_PRICE_NOTAX_STORE_02, SALE_PRICE_NOTAX_STORE_05, SALE_PRICE_NOTAX_STORE_06, SALE_PRICE_NOTAX_STORE_08, SALE_PRICE_NOTAX_STORE_07, SALE_PRICE_NOTAX_STORE_31, SALE_PRICE_NOTAX_STORE_32, SALE_PRICE_NOTAX_STORE_33, SALE_PRICE_NOTAX_STORE_34, SALE_PRICE_NOTAX_STORE_35, FIRST_DISC_PER_STORE_01, FIRST_DISC_PER_STORE_02, FIRST_DISC_PER_STORE_05, FIRST_DISC_PER_STORE_06, FIRST_DISC_PER_STORE_07, FIRST_DISC_PER_STORE_08, FIRST_DISC_PER_STORE_31, FIRST_DISC_PER_STORE_32, FIRST_DISC_PER_STORE_33, FIRST_DISC_PER_STORE_34, FIRST_DISC_PER_STORE_35, SECOND_DISC_PER_STORE_01, SECOND_DISC_PER_STORE_02, SECOND_DISC_PER_STORE_05, SECOND_DISC_PER_STORE_06, SECOND_DISC_PER_STORE_07, SECOND_DISC_PER_STORE_08, SECOND_DISC_PER_STORE_31, SECOND_DISC_PER_STORE_32, SECOND_DISC_PER_STORE_33, SECOND_DISC_PER_STORE_34, SECOND_DISC_PER_STORE_35 FROM ODBC_JHC.JHC_INVDATA WHERE FOREIGN_ITEM_CODE LIKE '{}' AND ITEM_MAIN_BARCODE LIKE '{}' AND ITEM_ID LIKE '{}'", mypRef, mypBarcode, mypId);
+    
+    let sql = format!("SELECT * FROM INVENTORY WHERE PRODUCTID LIKE '{}' AND BRAND LIKE '{}' AND PRODUCTNAME LIKE '{}'", mypID, mypBrand, mypName);
+    
     let conn = Connection::connect(username, password, database)?;
     let mut stmt = conn.statement(&sql.to_string()).build()?;
     let rows = stmt.query(&[])?;
 
-    let mut products : Vec<PRODUCT> = vec![];
+    let mut products : Vec<Product> = vec![];
     
     for row_result in rows {
         // print column values
 
         let row = row_result?;
-        let ITEM_ID : Option<String> = row.get("ITEM_ID")?;
-        let IS_ACTIVE : Option<String> = row.get("IS_ACTIVE")?;
+
+        let ProductID : Option<String> = row.get("PRODUCTID")?;
+        let ProductName : Option<String> = row.get("PRODUCTNAME")?;
+        let Quantity : Option<String> = row.get("QUANTITY")?;
+        let Price : Option<String> = row.get("PRICE")?;
+        let Brand : Option<String> = row.get("BRAND")?;
+        let BranchID : Option<String> = row.get("BRANCHID")?;
+        let SupplierID : Option<String> = row.get("SUPPLIERID")?;
+
         
 
-        let prod = PRODUCT {
-            ITEM_ID : ITEM_ID,
-            IS_ACTIVE : IS_ACTIVE,
-            CAN_BE_SOLD : CAN_BE_SOLD,
-            ITEM_DESC : ITEM_DESC,
-            ITEM_DESC_S : ITEM_DESC_S,
-            FOREIGN_ITEM_CODE : FOREIGN_ITEM_CODE,
-            ITEM_CAT : ITEM_CAT,
-            
+        let prod = Product {
+            ProductID : ProductID,
+            ProductName : ProductName,
+            Quantity : Quantity,
+            Price : Price,
+            Brand : Brand,
+            BranchID : BranchID,
+            SupplierID : SupplierID,            
         };
         products.push(prod);
     }
@@ -154,7 +124,7 @@ fn getProducts(params: Json<Params>) -> Result<Vec<PRODUCT>> {
     Ok(products)
 }
 
-
+/*
 fn newEmp(params: Json<Params>) -> Boolean {
     let username = "odbc_jhc";
     let password = "odbc_jhc";
@@ -204,3 +174,4 @@ fn newEmp(params: Json<Params>) -> Boolean {
 
     Ok(products)
 }
+*/
